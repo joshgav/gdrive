09e53152bae19ffc7ecd370ea99ce195f13cac85
Josh Gavant <joshgavant@gmail.com>
2019-03-27 13:45:33 -0700

    update dependencies, add search capability to list

--- bin/cli.js
+++ bin/cli.js
@@ -1,15 +1,3 @@
 #!/usr/bin/env node
 
-const authorize  = require('../lib/authorize.js');
-const operations = require('../lib/operations.js');
-const googleapis = require('googleapis');
-
-let apiClient = googleapis.drive('v3');
-
-authorize(authClient => {
-  googleapis.options({auth: authClient});
-  let op = process.argv[2];
-  let file = process.argv[3];
-  operations[op](apiClient, file);
-});
-
+require(`${__dirname}/../index.js`)
--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-require('./bin/cli.js');
+#!/usr/bin/env node
+
+const authorize  = require('./lib/authorize.js');
+const operations = require('./lib/operations.js');
+const {google} = require('googleapis');
+
+authorize(authClient => {
+	const drive = google.drive({
+		version: 'v3',
+		auth: authClient
+	});
+  let op   = process.argv[2];
+  let file = process.argv[3];
+	if (op && op.match(/(list|upload|download)/)) {
+		operations[op](drive, file);
+	} else {
+		usage()
+	}
+});
+
+function usage() {
+	console.log("Specify one of the following operations and a parameter.")
+	console.log("  list      [beginning-of-file-name]")
+	console.log("  upload    path-to-file")
+	console.log("  download  file-id")
+}
